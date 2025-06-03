1e61f7f8a0b537dfb4e87370e2fc7253be6293ed
Rahul Kumar <kumarrahul771996@gmail.com>
2025-06-03 18:28:17 +0530

    fix: update STATIC_ROOT configuration and add .gitkeep for static directory

--- myproject/settings.py
+++ myproject/settings.py
@@ -101,8 +101,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# This is where collectstatic will gather all static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # Default primary key field type
